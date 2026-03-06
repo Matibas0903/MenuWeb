2b938eeac8da8caa264b17d8980b718c397c8918
MarcoV1702 <villordomarco2020@gmail.com>
2026-03-06 17:48:07 -0300

    subo lo poco que hice

--- obtenerProductosBar.php
+++ obtenerProductosBar.php
@@ -1,2 +1,17 @@
 <?php
-require("conexion.php");
+    require("conexion.php");
+    header("Content-Type: application/json");
+
+    try{
+        $sql = "SELECT * FROM producto WHERE categoria = 11";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        echo json_encode($productos);
+    }catch(PDOException $e){
+        echo json_encode([
+            "error" => $e->getMessage()
+        ]);
+    }
+?>
