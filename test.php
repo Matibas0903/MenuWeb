d69c482e97d12de85f72e0538c972934164152e2
Matias Basterra <basterramatias2020@gmail.com>
2026-03-11 19:40:39 -0300

    test

---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+echo "HOST: " . getenv("MYSQLHOST") . "<br>";
+echo "PORT: " . getenv("MYSQLPORT") . "<br>";
+echo "DB: " . getenv("MYSQLDATABASE") . "<br>";
+echo "USER: " . getenv("MYSQLUSER") . "<br>";
